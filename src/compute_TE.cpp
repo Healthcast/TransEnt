1a9ab4f2cb23ec268bbfc44289d9cf48fd3517e5
Ghazale <ghazale.hnt@gmail.com>
2015-12-30 07:12:17 +0100

    copyright added

--- src/compute_TE.cpp
+++ src/compute_TE.cpp
@@ -1,9 +1,25 @@
 /**
 * @file compute_TE.cc
-* @brief This package is for computing Transfer Entropy estimation using three different methods
-* @author
-* @version
+* @brief This package is for computing Transfer Entropy estimation using two different methods
+* @author Ghazaleh Haratinezhad Torbati and Glenn Lawyer
+* @copyright Copyright (c) 2015 Max Planck Institute for Informatics and Ghazaleh 
+* Haratinezhad Torbati and Glenn Lawyer All Rights Reserved.
+* 
+* This program is free software; you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by the
+* Free Software Foundation; either version 2 of the License, or (at your
+* option) any later version.
+* 
+* This program is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+
+
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
